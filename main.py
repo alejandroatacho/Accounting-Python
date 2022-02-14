80ea58aa4a4301fdf03766d9adedf1f7026db081
Hinamizawa <themoonbunnys@gmail.com>
2022-02-14 15:21:57 -0400

    update function

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
+from fileinput import close
 import pandas as pd
 import os
+
 path = "files//Journal.xlsx"  # Cleaner version
-
-# place "r" before the path string to address special character, such as '\'. Don't forget to put the file name at the end of the path + '.xlsx'
+x = 0
+x_average = 0
+y = 0
+y_average = 0
 df = pd.read_excel(r'Accounting-Python//files//Journal.xlsx')
+formule1 = df['Product'].values
+formule2 = df['Units'].values * df['Cost'].values
+formule3 = df['Units_Sold'].values * df['Price'].values
 
+#unit * cost
+for i in df:
+    print(formule2[x])
+    x = x+1
+    #x_average = formule2[x] + x_average
+    if x == formule2.size:
+        break
+#unit_sold * price
+for i in df:
+    print(formule3[y])
+    y = y+1
+    if y == formule3.size:
+        break
+print(x_average)
+# read the excel column and puts it out
+'''for i in df:
+    print(formule1[x])
+    x = x+1
+    if x == formule1.size:
+        break
+    '''
+
+# check if the directory actually exist or can be read
 '''
 if os.path.exists(path):
     print("that location exists!")
@@ -14,4 +44,5 @@ else:
     print("that location does not exist")
 '''
 
-print(df)
+# print(df)
+# print(formule1)
